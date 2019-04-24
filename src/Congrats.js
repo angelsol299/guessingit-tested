8a5079e087f3a6f0314fec42df1c5c78388a153c
angel <angelsol299@hotmail.com>
2019-04-24 21:58:20 +0200

    first commits

---
+++ src/Congrats.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+/**
+ * Functional react component for congras message
+ * @function
+ * @returns {JSX.Element} - Rendered component (or null if sucess props is applied)
+ */
+
+const Congrats = () => {
+  return <div />;
+};
+
+export default Congrats;
