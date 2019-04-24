8545d0e4ba773e2a15b01fa159cc018f93c30364
angel <angelsol299@hotmail.com>
2019-04-24 22:21:53 +0200

    congrats components

--- src/Congrats.js
+++ src/Congrats.js
@@ -3,11 +3,21 @@ import React from "react";
 /**
  * Functional react component for congras message
  * @function
+ * @paaram {object}  props - React props.
  * @returns {JSX.Element} - Rendered component (or null if sucess props is false)
  */
 
-const Congrats = () => {
-  return <div />;
+const Congrats = (props) => {
+  return (
+   if(props.success){
+       <div data-test="component-congrats">
+           <span data-test="congrats-message">
+               Congratulations! You have guessed the word!!
+           </span>
+       </div>
+   }
+  
+  <div />);
 };
 
 export default Congrats;
--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
-
+import { findByTestAttr } from "../test/testUtils";
 import Congrats from "./Congrats";
+import { exportAllDeclaration } from "@babel/types";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
@@ -17,6 +18,18 @@ const setup = (props = {}) => {
   return shallow(<Congrats {...props} />);
 };
 
-it("renders without error", () => {});
-it("renders no text when success pros is false", () => {});
-it("renders non-empty congrats message when success prop is true", () => {});
+it("renders without error", () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, "component-congrats");
+  exportAllDeclaration(component.length).toBe(1);
+});
+it("renders no text when success pros is false", () => {
+  const wrapper = setup({ success: false });
+  const component = findByTestAttr(wrapper, "component-congrats");
+  expect(component.text).toBe("");
+});
+it("renders non-empty congrats message when success prop is true", () => {
+  const wrapper = setup({ success: true });
+  const message = findByTestAttr(wrapper, "congrats-message");
+  expect(message.text().length).not.toBe(0);
+});
