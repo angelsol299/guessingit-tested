73e848f60686ae5f1a6ee83df7a52dec886daefb
angel <angelsol299@hotmail.com>
2019-04-26 23:30:40 +0200

    adding redux and reducers

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,3 +1,9 @@
+/**
+ * @function sucessReducer
+ * @param {array} state - Array guessed words
+ * @param {object} action - action to be reduced
+ */
+
 export default (state, action) => {
   return null;
 };
