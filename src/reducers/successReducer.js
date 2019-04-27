a183fb7ac0439bc6808c37ab9d50ced37b8104a5
angel <angelsol299@hotmail.com>
2019-04-27 13:41:24 +0200

    sucess reducer testing

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,9 +1,15 @@
+import { actionTypes } from "../actions";
 /**
  * @function sucessReducer
  * @param {array} state - Array guessed words
  * @param {object} action - action to be reduced
  */
 
-export default (state, action) => {
-  return null;
+export default (state = false, action) => {
+  switch (action.type) {
+    case actionTypes.CORRECT_GUESS:
+      return true;
+    default:
+      return state;
+  }
 };
