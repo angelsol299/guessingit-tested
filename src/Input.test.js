e4da668a1e2c31876661b05da241be3285add8dd
angel <angelsol299@hotmail.com>
2019-04-27 13:56:07 +0200

    input js and input testing

---
+++ src/Input.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import { findByTestAttr } from "../test/testUtils";
+import Input from "./Input";
+
+/**
+ * Factory function to create a ShallowWrapper for the GuessedWords component.
+ * @function setup
+ * @param {object} initialState - Initial state for this setup.
+ * @returns {ShallowWrapper}
+ */
+
+const setup = (initialState = {}) => {
+  const wrapper = shallow(<Input />);
+  console.log(wrapper.debug());
+};
+
+describe("render", () => {
+  describe("word has not been guessed", () => {
+    it("renders component without error", () => {});
+    it("renders input box", () => {});
+    it("renders submit button", () => {});
+  });
+  describe("word has been guessed", () => {
+    it("renders component without error", () => {});
+    it("does not render input box", () => {});
+    it("does not render submit button", () => {});
+  });
+});
+describe("update state", () => {});
