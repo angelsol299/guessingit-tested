e03e45949ff0b86a887ae0578f907c28e1f65a62
angel <angelsol299@hotmail.com>
2019-04-27 13:22:29 +0200

    actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,12 @@
 export const actionTypes = {
   CORRECT_GUESS: "CORRECT_GUESS"
 };
+
+/**
+ * @function correctGuess
+ * @returns {object} - Action object with type "CORRECT_GUESS"
+ */
+
+export const correctGuess = () => {
+  return {};
+};
---
+++ src/actions/index.test.js
@@ -0,0 +1,5 @@
+import { correctGuess, actionTypes } from "./";
+
+describe("correctGuess", () => {
+  it("returns an action with type `CORRECT_GUESS`", () => {});
+});
