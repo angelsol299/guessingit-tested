cd0a29be26a914be034a53ead88b40f75264d754
angel <angelsol299@hotmail.com>
2019-04-27 23:50:52 +0200

    redux thunk

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,5 +1,5 @@
 import checkPropTypes from "check-prop-types";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../src/reducers";
 import { middlewares } from "../src/configureStore";
 
@@ -12,7 +12,10 @@ import { middlewares } from "../src/configureStore";
  */
 
 export const storeFactory = initialState => {
-  return createStore(rootReducer, initialState);
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(
+    createStore
+  );
+  return createStoreWithMiddleware(rootReducer, initialState);
 };
 /**
  * Return node(s) with the given data-test attribute
