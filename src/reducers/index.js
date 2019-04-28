5b8d79246d2bd6d70bc9a1b0380f44099123fe60
angel <angelsol299@hotmail.com>
2019-04-28 17:36:15 +0200

    reducer and action creators

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 import success from "./successReducer";
 import guessedWords from "./guessWordsReducer";
-import secretWord from "./guessWordsReducer";
+import secretWord from "./secretWordReducer";
 
 export default combineReducers({
   success,
