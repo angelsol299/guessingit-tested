f42cab78f2405db621b9eaed195e4c747e83b868
angel <angelsol299@hotmail.com>
2019-04-28 16:56:10 +0200

    more tests git push

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import success from "./successReducer";
 import guessedWords from "./guessWordsReducer";
+import secretWord from "./guessWordsReducer";
 
 export default combineReducers({
   success,
-  guessedWords
+  guessedWords,
+  secretWord
 });
