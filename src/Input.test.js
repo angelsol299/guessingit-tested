e14ec648e3a063b5c2a47ae5a9bce3ff2c5a9aca
angel <angelsol299@hotmail.com>
2019-04-29 23:17:59 +0200

    input submit click simulation

--- src/Input.test.js
+++ src/Input.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 
 import { findByTestAttr, storeFactory } from "../test/testUtils";
-import Input from "./Input";
+import Input, { UnconnectedInput } from "./Input";
 
 /**
  * Factory function to create a ShallowWrapper for the GuessedWords component.
@@ -71,6 +71,26 @@ describe("redux props", () => {
     const wrapper = setup();
     const guessWordProp = wrapper.instance().props.guessWord;
     expect(guessWordProp).toBeInstanceOf(Function);
+  });
+});
+
+describe("`guessWord` action creator call", () => {
+  it("calls `guessWord` when button is clicked", () => {
+    const guessWordMock = jest.fn();
+    const props = {
+      guessWord: guessWordMock
+    };
+    //setup app component with guessWordMock as the guessWord prop
+    const wrapper = shallow(<UnconnectedInput {...props} />);
+
+    //simulate clicked
+    const submitButton = findByTestAttr(wrapper, "submit-button");
+    submitButton.simulate("click");
+
+    //check to see if mock ran
+    const guessWordCallCount = guessWordMock.mock.calls.length;
+
+    expect(guessWordCallCount).toBe(1);
   });
 });
 
